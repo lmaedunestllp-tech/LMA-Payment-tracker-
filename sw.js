048a1ed63bb90acdad271f213a4d2ae22d3ab590
lmaedunestllp-tech <lmaedunestllp@gmail.com>
2026-05-14 17:44:21 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+// LMA EduNest Service Worker
+const CACHE_NAME = 'lma-edunest-v1';
+const urlsToCache = [
+  '/LMA-Payment-tracker-/',
+  '/LMA-Payment-tracker-/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => 
+      caches.match(event.request)
+    )
+  );
+});
